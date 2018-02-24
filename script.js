f5585d4bc481816e2fc6c48ebcb5854b66b72ecc
Abdo-A <abdopoetry@gmail.com>
2018-02-24 22:23:51 +0200

    =Added skeleton

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
-let p1=document.querySelector('p1');
-let t1=document.querySelector('t1');
-let p2=document.querySelector('p2');
-let t2=document.querySelector('t2');
-let p3=document.querySelector('p3');
-let t3=document.querySelector('t3');
-let p4=document.querySelector('p4');
-let t4=document.querySelector('t4');
+function handleSelect(){
+    body.style.backgroundColor="red";
+}
 
+function onSubmit(){
+    let p1=document.getElementById('p1');
+    let t1=document.getElementById('t1');
+    let p2=document.getElementById('p2');
+    let t2=document.getElementById('t2');
+    let p3=document.getElementById('p3');
+    let t3=document.getElementById('t3');
+    let p4=document.getElementById('p4');
+    let t4=document.getElementById('t4');
+    let answer=document.getElementById('answer');
+    let body=document.querySelector('body');
+    let criteria=document.getElementById('criteria');
+    
+    if(criteria.value==='firstComeFirstServed'){
+        firstComeFirstServed();
+    } else if(criteria.value==='shortedJobFirst'){
+        shortedJobFirst();
+    } else if(criteria.value==='shortedJobFirstPreemtive'){
+        shortedJobFirstPreemtive();
+    } else if(criteria.value==='priority'){
+        priority();
+    } else if(criteria.value==='priorityPreemtive'){
+        priorityPreemtive();
+    } else if(criteria.value==='RR'){
+        RR();
+    }
+}
+
+function firstComeFirstServed(){
+    answer.innerHTML=p1.value;
+}
+function shortedJobFirst(){
+    
+}
+function shortedJobFirstPreemtive(){
+    
+}
+function priority(){
+    
+}
+function priorityPreemtive(){
+    
+}
+function RR(){
+    
+}
