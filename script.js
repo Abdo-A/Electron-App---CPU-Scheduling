dd28ace6ab10b22ead73fd79fb53b1e73e80c01b
Abdo-A <abdopoetry@gmail.com>
2018-02-24 20:33:02 +0200

    =first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let p1=document.querySelector('p1');
+let t1=document.querySelector('t1');
+let p2=document.querySelector('p2');
+let t2=document.querySelector('t2');
+let p3=document.querySelector('p3');
+let t3=document.querySelector('t3');
+let p4=document.querySelector('p4');
+let t4=document.querySelector('t4');
+
